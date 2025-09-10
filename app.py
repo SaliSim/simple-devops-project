1f0728ba632f3d573edb826a1d9fbe7d54bed2ba
SaliSim <salisimiyu319@gmail.com>
2025-09-10 12:01:35 +0300

    Small change to detect if Argocd will pick changes

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = flask(__name__)
 @app.route("/")
 def get_current_time():
     current_time = time.strftime("%Y-%M-%D %H:%M:%S", time.localtime())
-    return f"The Current Time:{current_time}"
+    return f"The Current Time today is:{current_time}"
 
 if  __name__ == "__main__":
     app.run(host="0.0.0.0",port=8080)
