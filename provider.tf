ecf1c19c7c2e5ab2e1307f3b0ee2f9ac0968e69b
SaliSim <salisimiyu319@gmail.com>
2025-09-23 12:41:43 +0300

    updating provider.tf file

--- provider.tf
+++ provider.tf
@@ -1,12 +1,15 @@
 provider "kubernetes" {
-    host = minikube_cluster.minikube_docker.host
-    client_certificates = minikube_cluster.minikube_docker.client_certificates
-    client_key = minikube_cluster.minikube_docker.client_key
-    cluster_ca_certificate = minikube_cluster.minikube_docker.cluster_ca_certificate
+  host                   = minikube_cluster.minikube_docker.host
+  client_certificate     = minikube_cluster.minikube_docker.client_certificate
+  client_key             = minikube_cluster.minikube_docker.client_key
+  cluster_ca_certificate = minikube_cluster.minikube_docker.cluster_ca_certificate
 }
 
 provider "helm" {
-    kubernetes = {
-        config_path = pathexpand("~/.kube/config")
-    }
+  kubernetes {
+    host                   = minikube_cluster.minikube_docker.host
+    client_certificate     = minikube_cluster.minikube_docker.client_certificate
+    client_key             = minikube_cluster.minikube_docker.client_key
+    cluster_ca_certificate = minikube_cluster.minikube_docker.cluster_ca_certificate
+  }
 }
